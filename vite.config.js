00994a6664182386e8199977abfddb905f3e12a6
manish0909 <31737740+manish0909@users.noreply.github.com>
2026-03-12 18:54:43 +0530

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
-```
-4. Press **Ctrl+S**
-
----
-
-Your file structure should look like:
-```
-📁 siddipet-bazaar
-   📄 index.html
-   📄 package.json
-   📄 vite.config.js
-   📁 src
-      📄 main.jsx
-      📄 App.jsx
